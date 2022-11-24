0b491394ad56fc6c9d83075e87cb2ccfb87ecf96
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-24 12:17:00 +0600

    add agent

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import HeroSection from "./HeroSection/HeroSection";
+import ExtraSections from "./ExtraSection/ExtraSections";
 
 const Home = () => {
+   
   return (
     <div>
       <HeroSection></HeroSection>
+      <ExtraSections></ExtraSections>
+      
     </div>
   );
 };
