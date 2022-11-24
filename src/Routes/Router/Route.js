1119033a21fa1d43a24442ef47aea9e7dd12ae13
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-24 16:36:44 +0600

    fix some error

--- src/Routes/Router/Route.js
+++ src/Routes/Router/Route.js
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../Pages/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
     {path:'/', element:<Main></Main>,children:[
         {path:'/', element:<Home></Home>},
+        {path:'/signup', element:<SignUp></SignUp>},
+        {path:'/login',element:<Login></Login>}
         
     ]}
 ])
