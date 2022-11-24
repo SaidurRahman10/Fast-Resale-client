4e0950f1cfd480877032ab332ce84fc7a9c1d5a3
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-24 16:56:18 +0600

    add blog

--- src/Routes/Router/Route.js
+++ src/Routes/Router/Route.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -8,7 +9,8 @@ export const router = createBrowserRouter([
     {path:'/', element:<Main></Main>,children:[
         {path:'/', element:<Home></Home>},
         {path:'/signup', element:<SignUp></SignUp>},
-        {path:'/login',element:<Login></Login>}
+        {path:'/login',element:<Login></Login>},
+        {path:'/blog',element:<Blog></Blog>}
         
     ]}
 ])
